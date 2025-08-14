e4f6451bb25c140c7e0e1f567dbb10980f5bedca
akhioafk <aidosulyakzhol@gmail.com>
2025-08-14 06:32:50 +0500

    path/body params

--- app/main.py
+++ app/main.py
@@ -9,10 +9,17 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+# Path params (Required)
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+# Query params (NotRequired)
+@app.get("/users")
+def list_users(skip: int = 0, limit: int = 10):
+    return {"skip": skip, "limit": limit}
+
+# Request Body (When need to pass JSON)
 @app.post("/users")
 def create_user(user: User):
     return {"message": f"{user.id}: User {user.name} is {user.age} years old g."}
